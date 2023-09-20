708f02b5ddbfc57f2ac9b6094901247a2a99c364
Maxime Oudin <oudinmaxime@gmail.com>
2023-09-20 08:35:36 +0200

    routes and controllers created

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  root to: "pages#home"
+  # root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "music#index"
 
-  # resources music
+  resources :artist, only: %i[index show]
+  resources :music, only: %i[index show]
 end
