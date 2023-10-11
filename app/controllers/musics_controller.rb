2a7dcc83d6cef461181eb9f9e304664dff55e035
Maxime Oudin <oudinmaxime@gmail.com>
2023-10-11 15:49:04 +0200

    search bar is working well and no more error message if query is not existing

--- app/controllers/musics_controller.rb
+++ app/controllers/musics_controller.rb
@@ -1,19 +1,20 @@
 class MusicsController < ApplicationController
   def show
     @music_array = []
-    query = params[:query]
+    @query = params[:query]
+    @query = nil ? 'kiss the rain' : params[:query]
     @shazam_service = ShazamService.new
-    @music_hash = @shazam_service.search_music(query)
+    @music_hash = @shazam_service.search_music(@query)
     @music_hashed = JSON.parse(@music_hash)
     render 'musics/index'
   end
 
   def index
     @music_array = []
-    query = params[:query]
-    query == nil ? 'kiss the rain' : params[:query]
+    @query = params[:query]
+    @query = 'nil' ? 'kiss the rain' : params[:query]
     @shazam_service = ShazamService.new
-    @music_hash = @shazam_service.search_music(query)
+    @music_hash = @shazam_service.search_music(@query)
     @music_hashed = JSON.parse(@music_hash)
   end
 
