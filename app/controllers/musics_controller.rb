ab59ba88b75f1d3342b1241a340335e6f621cab1
Maxime Oudin <oudinmaxime@gmail.com>
2023-10-11 16:05:24 +0200

    home page created with one card example to work on front

--- app/controllers/musics_controller.rb
+++ app/controllers/musics_controller.rb
@@ -17,12 +17,4 @@ class MusicsController < ApplicationController
     @music_hash = @shazam_service.search_music(@query)
     @music_hashed = JSON.parse(@music_hash)
   end
-
-  private
-
-  def generate_random_track_ids(count)
-    @random_track_ids = []
-    count.times { @random_track_ids << rand(42_222_222..55_555_555) }
-    @random_track_ids
-  end
 end
